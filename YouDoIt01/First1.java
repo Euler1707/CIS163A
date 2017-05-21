57ff6319faf61ae49fc9a7e284b6fc6e6cb1616c
Euler1707 <edcard29@gmail.com>
2017-05-21 11:15:18 -0500

    Files

---
+++ YouDoIt01/First1.java
@@ -0,0 +1,9 @@
+public class First1
+{
+   public static void main(String[] args)
+   {
+      System.out.println("My new and improved");
+      System.out.println("Java application");
+   }
+}
+   
---
+++ YouDoIt01/Hello2.java
@@ -0,0 +1,15 @@
+//Filename Hello2.java
+//Written by Edgar Colin
+//Written on 07/26/2016
+
+
+
+public class Hello2
+{
+   public static void main(String[] args)
+   {
+      System.out.println("Hello, World!");
+   }
+}
+/*This class demosntrates the use of the println() 
+method to print the message Hello, World! */
